9c993a2d90d29180f159f97279f11098ab371529
BadokMobile\Badok <badokk@gmail.com>
2016-11-23 13:51:59 +0100

    Stworzono szkic modelu bazy danych, skrypt ją generujący oraz zalążek klasy ją kontrolującej

---
+++ Agile16_Komis/src/wwwroot/Database/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agile16_Komis.src.wwwroot.Database
+{
+    public static class DatabaseController
+    {
+        public static void GenerateDatabase()
+        {
+
+        }
+
+        public static void InitiateValues()
+        {
+
+        }
+
+    }
+}
