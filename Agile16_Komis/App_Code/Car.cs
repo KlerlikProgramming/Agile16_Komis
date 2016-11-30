c8aca530ad0a5e03cf97e3d631d20390beb84ad5
DESKTOP-I0LBIAB\Krimek <krzysiek.k366@gmail.com>
2016-11-30 13:17:54 +0100

    Model danych do wyświetlania

---
+++ Agile16_Komis/App_Code/Car.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace Agile16_Komis.App_Code
+{
+    public class Car
+    {
+        List<string> photo;
+        public Car()
+        {
+            photo = new List<string>();
+        }
+
+        private int iD;
+        public int ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+        private string mark;
+        public string Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+        private string model;
+        private string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        private int yearProduction;
+        public int YeatProduction
+        {
+            get { return yearProduction; }
+            set { yearProduction = value; }
+        }
+        private string engineType;
+        public string EngineType
+        {
+            get { return engineType; }
+            set { engineType = value; }
+        }
+        private string transmissionModel;
+        public string TransmissionModel
+        {
+            get { return transmissionModel; }
+            set { transmissionModel = value; }
+        }
+        private int tranmissionGears;
+        public int TransmissionGears
+        {
+            get { return tranmissionGears; }
+            set { tranmissionGears = value; }
+        }
+        private string fuelType;
+        public string FuelType
+        {
+            get { return fuelType; }
+            set { fuelType = value; }
+        }
+        private string chassis;
+        public string Chassis
+        {
+            get { return chassis; }
+            set { chassis = value; }
+        }
+        private string color;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public Car This
+        {
+            get { return this; }
+        }
+
+        public override string ToString()
+        {
+            string data = Mark + " " + Model + " " + YeatProduction.ToString() + " " + EngineType + " " + TransmissionModel + " " + TransmissionGears.ToString() + " " + FuelType + " " + Chassis + " " + Color;
+            foreach(string item in photo)
+            {
+                data += " " + item;
+            }
+            return data;
+        }
+    }
+}
