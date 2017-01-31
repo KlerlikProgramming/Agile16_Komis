f9fa876829553d5ef1bbed4237c4cf349129602a
BadokMobile\Badok <badokk@gmail.com>
2017-02-01 00:57:33 +0100

    Dodano redirect

--- Agile16_Komis/src/wwwroot/AddOffer.aspx.cs
+++ Agile16_Komis/src/wwwroot/AddOffer.aspx.cs
@@ -17,11 +17,12 @@ namespace Agile16_Komis.src
         {
             App_Code.Car car = new App_Code.Car() { Model = modelTextBox.Text, Brand = brandTextBox.Text, YearProduction = Convert.ToInt32(yearProductionTextBox.Text), EngineType = engineTypeTextBox.Text, EngineFuel = Convert.ToInt32(engineFuelTextBox.Text), TransmissionModel = transmissionModelTextBox.Text, TransmissionGears = Convert.ToInt32(transmissionGearsTextBox.Text), Chassis = chassisTextBox.Text, Color = colorTextBox.Text, Price = Convert.ToInt32(priceTextBox.Text) };
             App_Code.ListCar.AddCar(car);
+            Server.Transfer("index.aspx");
         }
 
         protected void CancelButton_Click(object sender, EventArgs e)
         {
-
+            Server.Transfer("index.aspx");
         }
     }
 }
